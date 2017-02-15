57a126143c75b65ec09d6e2b261b36bfa21b0b84
c00z <zcusimano@gmail.com>
2017-02-15 14:11:35 -0800

    todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'site#index'
+
+  namespace :api, defaults: { format: :json } do
+    resources :todos, except: [:new, :edit]
+  end
+
   get '*path', to: 'site#index'
 
 end
